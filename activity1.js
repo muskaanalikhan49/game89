66471f0feb687c12b138ec4f96c04344b9fbffe3
muskaanalikhan49 <78203866+muskaanalikhan49@users.noreply.github.com>
2021-04-09 16:27:47 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,17 @@
+var score=0;
+
+function updateScore()
+{
+    score=score + 1;
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+
+function saveScore()
+{
+    localStorage.setItem("score", score);
+}
+
+function nextPage()
+{
+    window.location = "activty_2.html";
+}
